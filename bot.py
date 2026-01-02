bd792d157b823fa42eeb3a6d9e4dd6ec4cd18e5f
h4rdl <kapa562@yandex.ru>
2026-01-02 20:04:32 +0300

    version 1.0.0

--- bot.py
+++ bot.py
@@ -124,6 +124,6 @@ if __name__ == '__main__':
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("help", help_command))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
-
+    
     logger.info("Бот запущен. Ожидание сообщений...")
     app.run_polling()
